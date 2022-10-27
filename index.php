819b8b930a42b37d57c5a0e848886fe03c461956
Diego Muniz da Silva <1008321@UNIDAVI.local>
2022-10-27 19:26:13 -0300

    adicionado start da dessão no index

--- index.php
+++ index.php
@@ -14,37 +14,69 @@
             session_start();
         }
     }
-
-    include_once 'lib/conexao.php';
-    include_once 'lib/sql.php';
-    include_once 'lib/autenticar.php';
-
-    if (isset($_SESSION['autenticacao'])) {
-        if (isset($_GET['pagina'])) {
-            $SQL - "select * from paginas where id - :id";
-
-            $consulta = $conn->prepare($SQL);
-            
-            $linha = $consulta->fetch();
-
-            if ($consulta->rowcount() > 0) {
-                include 'menu.php';
-                include $linha['src'];
-            } else {
-                include 'menu.php';
-                include '404.php';
-            }
-        } else {
-            include 'menu.php';
-            include 'home.php';
-        }
-    } else {
-        include 'login.php';
-    }
-
-    if (isset($_SESSION['debug'])) {
-        if ($_SESSION['debug'] == true) {
-            include 'lib/debug.php';
-        }
-    }
 ?>
+
+<h3>My Commerce</h3>
+
+<form method="post">
+    <center>
+        <input type="text" name="value" value="<?php echo $value?>" readonly>
+        <br>
+        <br>
+        <table border="1">
+            <tr>
+                <td>
+                    <input type="submit" name="submit" value="7">
+                </td>
+                <td>
+                    <input type="submit" name="submit" value="8">
+                </td>
+                <td>
+                    <input type="submit" name="submit" value="9">
+                </td>
+                <td>
+                    <input type="submit" name="op" value="/">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="submit" value="4">
+                </td>
+                <td>
+                    <input type="submit" name="submit" value="5">
+                </td>
+                <td>
+                    <input type="submit" name="submit" value="6">
+                </td>
+                <td>
+                    <input type="submit" name="op" value="*">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="submit" value="1">
+                </td>
+                <td>
+                    <input type="submit" name="submit" value="2">
+                </td>
+                <td>
+                    <input type="submit" name="submit" value="3">
+                </td>
+                <td>
+                    <input type="submit" name="op" value="-">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="submit" value="0">
+                </td>
+                <td colspan="2">
+                    <input type="submit" name="equals_to" value="=">
+                </td>
+                <td>
+                    <input type="submit" name="op" value="+">
+                </td>
+            </tr>
+        </table>
+    </center>
+</form>
